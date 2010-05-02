0b69f6f67889ef41f29f48979552d71c299a112b
janoliver <janoliver@4b57dc50-a839-803c-ad60-7dbd33f65f1d>
2010-05-02 08:57:34 +0000

    initial commit

---
+++ trunk/bin/doctrine.php
@@ -0,0 +1,66 @@
+<?php
+
+define('APPLICATION_ENV', 'development');
+ 
+define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+// Doctrine and Symfony Classes
+require_once 'Doctrine/Common/ClassLoader.php';
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine', APPLICATION_PATH . '/../library');
+$classLoader->register();
+$classLoader = new \Doctrine\Common\ClassLoader('Symfony', APPLICATION_PATH . '/../library/vendor');
+$classLoader->register();
+$classLoader = new \Doctrine\Common\ClassLoader('Entities', APPLICATION_PATH . '/models');
+$classLoader->setNamespaceSeparator('_');
+$classLoader->register();
+
+// Zend Components
+require_once 'Zend/Application.php';
+ 
+// Create application
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+
+// bootstrap doctrine
+$application->getBootstrap()->bootstrap('doctrine');
+$em = $application->getBootstrap()->getResource('doctrine');
+
+// generate the Doctrine HelperSet
+$helperSet = new \Symfony\Components\Console\Helper\HelperSet(array(
+    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
+));
+
+// start the CLI
+$cli = new \Symfony\Components\Console\Application('Doctrine Command Line Interface', Doctrine\Common\Version::VERSION);
+$cli->setCatchExceptions(true);
+$cli->setHelperSet($helperSet);
+$cli->addCommands(array(
+    // DBAL Commands
+    new \Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
+    new \Doctrine\DBAL\Tools\Console\Command\ImportCommand(),
+
+    // ORM Commands
+    new \Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
+    new \Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
+
+));
+$cli->run();
